f6144db22f7f080924a69a09cce22038c780728f
MLee21 <minnie.lee89@gmail.com>
2015-03-11 16:56:47 -0600

    added test fixtures and set up rake test.

---
+++ test/test_test.rb
@@ -0,0 +1,21 @@
+require 'csv'
+gem "minitest"
+require "minitest"
+require "minitest/autorun"
+require "minitest/pride"
+require_relative '../test/data/customers.csv'
+
+class CustomerParserTest < MiniTest::Test
+
+
+
+
+
+
+
+
+
+
+
+
+end
