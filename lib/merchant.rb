f06088fa2c8743669a14d3985971c1d25bcc76c5
MLee21 <minnie.lee89@gmail.com>
2015-03-15 16:26:55 -0600

    merge conflicts.

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -11,7 +11,11 @@ class Merchant
     @name       = data[:name]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
+<<<<<<< HEAD
     @repo       = repo
+=======
+    @repo       = parent
+>>>>>>> cb08f184069938553893913aa709220d57da3b6e
   end
 
   def items
@@ -21,5 +25,9 @@ class Merchant
   def invoices
     repo.find_invoices(id)
   end 
+<<<<<<< HEAD
 end
 
+=======
+end
+>>>>>>> cb08f184069938553893913aa709220d57da3b6e
